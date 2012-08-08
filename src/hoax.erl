070c5e008311f22472c62296b142dc43cf3b6d2f
Matt Campbell <xenolinguist@gmail.com>
2012-08-07 23:41:07 -0500

    Temp commit

---
+++ src/hoax.erl
@@ -0,0 +1,39 @@
+-module(hoax).
+
+-export([stub/1, stub/2,
+        %fake/2,
+        stub_a/2, stub_a/3,
+        expect/3,
+        and_return/1,
+        and_throw/1,
+        unload/0
+    ]).
+
+stub(M) -> stub(M, []).
+stub(ModuleName, Expectations) ->
+    ModName = list_to_atom(atom_to_list(ModuleName) ++ "_hoax"),
+    Funcs = get_exports(ModuleName),
+    compile(hoax_ast:module(ModName, Funcs, Expectations)).
+
+stub_a(B, M) -> stub_a(B, M, []).
+stub_a(Behaviour, ModuleName, Expectations) ->
+    Funcs = Behaviour:behaviour_info(callbacks),
+    compile(hoax_ast:module(ModuleName, Funcs, Expectations)).
+
+expect(Func, Args, Action) -> {Func, Args, Action}.
+
+and_return(Value) -> {return, Value}.
+
+and_throw(Error) -> {throw, Error}.
+
+unload() -> ok.
+
+%%%%%%%%%%%%%
+
+compile(AST) ->
+    Forms = erl_syntax:revert_forms(AST),
+    {ok, Mod, Bin} = compile:forms(Forms),
+    code:load_binary(Mod, "", Bin).
+
+get_exports(ModuleName) ->
+    [ E || E = {F,_} <- ModuleName:module_info(exports), F =/= module_info ].
---
+++ src/hoax_ast.erl
@@ -0,0 +1,86 @@
+-module(hoax_ast).
+
+-compile([export_all]).
+
+-import(erl_syntax, [
+        abstract/1,
+        application/2,
+        arity_qualifier/2,
+        atom/1,
+        attribute/2,
+        clause/3,
+        function/2,
+        integer/1,
+        list/1,
+        module_qualifier/2,
+        tuple/1,
+        underscore/0,
+        variable/1
+    ]).
+
+% -type(ast() :: erl_syntax:syntax_tree()).
+% -type(func() :: {FA::{atom(), integer()}, {Export::ast(), Clauses::[ast()]}}).
+
+module(Name, Funcs, Expectations) ->
+    FinalClauses = lists:foldl(
+        fun add_expectation/2,
+        default_clauses(Name, Funcs, fun unexpected_invocation/2),
+        Expectations),
+
+    Exports = [make_export(K) || {K, _} <- dict:fetch_keys(FinalClauses)],
+
+    [ module_attr(Name), export_attr(Exports) ] ++
+    [ function(atom(F), Clauses) || {{F,_}, Clauses} <- FinalClauses ].
+
+default_clauses(Name, Funcs, DefaultClauseFun) ->
+    lists:foldl(
+        fun(F,Acc) ->
+            dict:store(F, [DefaultClauseFun(Name, F)], Acc)
+        end, dict:new(), Funcs).
+
+module_attr(Name) ->
+    attribute(atom(module), [atom(Name)]).
+
+export_attr(Funcs) ->
+    attribute(atom(export), [list(Funcs)]).
+
+make_export({F, A}) ->
+    arity_qualifier(atom(F), integer(A)).
+
+unexpected_invocation(_, {_, A}) ->
+    Args = lists:map(fun(_) -> underscore() end, lists:seq(1,A)),
+    clause(Args, [], [atom(ok)]).
+
+unexpected_invocation_exception(M, {F, A}) ->
+    Args = variables_for_arity(A),
+    Exception = tuple([atom(unexpected_invocation), m_f_args(M, F, Args)]),
+    clause(Args, [], [throw_exception(Exception)]).
+
+add_expectation({Func, Args, Action}, FuncDict) ->
+    Key = {Func, length(Args)},
+    case dict:find(Key, FuncDict) of
+        error ->
+            erlang:throw({no_such_function, Key});
+        {ok, Clauses} ->
+            Clause = expected_invocation(Args, Action),
+            dict:store(Key, [Clause|Clauses], FuncDict)
+    end.
+
+expected_invocation(Args, Body) ->
+    clause([abstract(Arg) || Arg <- Args], [], [function_body(Body)]).
+
+function_body({return, Value}) ->
+    abstract(Value);
+function_body({throw, Error}) ->
+    throw_exception(abstract(Error)).
+
+variables_for_arity(Arity) ->
+    [ variable("V"++integer_to_list(Num)) || Num <- lists:seq(1,Arity) ].
+
+throw_exception(Exception) ->
+    Throw = module_qualifier(atom(erlang), atom(throw)),
+    application(Throw, [Exception]).
+
+m_f_args(M, F, Args) ->
+    tuple([atom(M), atom(F), list(Args)]).
+
