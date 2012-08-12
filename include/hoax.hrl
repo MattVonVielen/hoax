50a96b3303baf23e3976043487ddabdd1f5fbf41
Matt Campbell <xenolinguist@gmail.com>
2012-08-12 11:21:12 -0500

    Add fixture macros, convert tests to fixture

--- include/hoax.hrl
+++ include/hoax.hrl
@@ -1,19 +1,60 @@
 -define(HOAX_API, [
-    stub/1,
-    stub/2,
-    stub_a/2,
-    stub_a/3,
-    fake/2,
-    expect/2,
-    expect/3,
-    and_return/1,
-    and_throw/1,
-    unload/0,
-    unload/1
-  ]).
+        stub/1,
+        stub/2,
+        stub_a/2,
+        stub_a/3,
+        fake/2,
+        expect/2,
+        expect/3,
+        and_return/1,
+        and_throw/1,
+        unload/0,
+        unload/1
+    ]).
 
 -ifdef(IN_HOAX_ERL).
 -export(?HOAX_API).
 -else.
 -import(hoax, ?HOAX_API).
 -endif.
+
+-define(HF_NAME, hoax_fixture_test_).
+-define(HF_EMPTY_SETUP, fun() -> ok end).
+-define(HF_EMPTY_TEARDOWN, fun(_) -> ok end).
+-define(HF_TEST_DECL, fun ?MODULE:F/0).
+-define(HF_TEST_DECL_WITH_ARG, {with, [fun ?MODULE:F/1]}).
+
+-define(HF_FIXTURE_BASE(Setup, Teardown, TestArity, TestDecl),
+    ?HF_NAME() ->
+        {foreach,
+            fun() -> Setup() end,
+            fun(X) -> Teardown(X), hoax:unload() end,
+            [ TestDecl ||
+                {F, A} <- ?MODULE:module_info(exports),
+                A == TestArity,
+                F =/= ?HF_NAME,
+                F =/= module_info,
+                F =/= test,
+                F =/= Setup,
+                F =/= Teardown
+            ]
+        }
+).
+
+-define(HOAX_FIXTURE(Setup, Teardown),
+    ?HF_FIXTURE_BASE(Setup, Teardown, 0, ?HF_TEST_DECL)
+).
+
+-define(HOAX_FIXTURE,
+    ?HF_FIXTURE_BASE(?HF_EMPTY_SETUP, ?HF_EMPTY_TEARDOWN, 0, ?HF_TEST_DECL)
+).
+
+-define(HOAX_FIXTURE_WITH_ARG(Setup, Teardown),
+    ?HF_FIXTURE_BASE(Setup, Teardown, 1, ?HF_TEST_DECL_WITH_ARG)
+).
+
+-define(HOAX_FIXTURE_WITH_ARG,
+    ?HF_FIXTURE_BASE(?HF_EMPTY_SETUP, ?HF_EMPTY_TEARDOWN, 1,
+        ?HF_TEST_DECL_WITH_ARG)
+).
+
--- test/hoax_test.erl
+++ test/hoax_test.erl
@@ -10,90 +10,90 @@
 -define(REAL_MOD, hoax_test_module).
 -define(REAL_FUN, exported_function).
 
-stub_should_throw_when_module_cannot_be_loaded_test() ->
+?HOAX_FIXTURE.
+
+stub_should_throw_when_module_cannot_be_loaded() ->
     ExpectedError = {no_such_module_to_stub, ?FAKE_MOD},
     ?assertError(ExpectedError, stub(?FAKE_MOD, [])).
 
-stub_should_throw_when_module_does_not_have_expected_function_test() ->
+stub_should_throw_when_module_does_not_have_expected_function() ->
     ExpectedError = {no_such_function_to_stub, {?FAKE_FUN, 0}},
     ?assertError(ExpectedError,
         stub(?REAL_MOD, [
                 expect(?FAKE_FUN, [])
             ])).
 
-fake_should_throw_when_module_exists_test() ->
+fake_should_throw_when_module_exists() ->
     ExpectedError = {module_exists, ?REAL_MOD},
     ?assertError(ExpectedError, fake(?REAL_MOD, [])).
 
-unload_should_remove_faked_module_from_vm_test() ->
+unload_should_remove_faked_module_from_vm() ->
     fake(?FAKE_MOD, []),
+
     unload(?FAKE_MOD),
+
     ?assertMatch({error,nofile}, code:ensure_loaded(?FAKE_MOD)).
 
-fake_should_return_expected_value_when_args_match_test() ->
+fake_should_return_expected_value_when_args_match() ->
     fake(?FAKE_MOD, [
             expect(?FAKE_FUN,[arg1,arg2],
                 and_return(a_result))
         ]),
 
-    ?assertEqual(a_result, ?FAKE_MOD:?FAKE_FUN(arg1, arg2)),
-    unload(?FAKE_MOD).
+    ?assertEqual(a_result, ?FAKE_MOD:?FAKE_FUN(arg1, arg2)).
 
-fake_should_throw_expected_value_when_args_match_test() ->
+fake_should_throw_expected_value_when_args_match() ->
     fake(?FAKE_MOD, [
             expect(?FAKE_FUN,[arg1,arg2],
                 and_throw(an_error))
         ]),
 
-    ?assertError(an_error, ?FAKE_MOD:?FAKE_FUN(arg1, arg2)),
-    unload(?FAKE_MOD).
+    ?assertError(an_error, ?FAKE_MOD:?FAKE_FUN(arg1, arg2)).
 
-fake_should_return_default_value_when_args_do_not_match_test() ->
+fake_should_return_default_value_when_args_do_not_match() ->
     fake(?FAKE_MOD, [
             expect(?FAKE_FUN,[arg1,arg2],
                 and_return(a_result))
         ]),
 
-    ?assertEqual(ok, ?FAKE_MOD:?FAKE_FUN(arg1, not_matching_arg)),
-    unload(?FAKE_MOD).
+    ?assertEqual(ok, ?FAKE_MOD:?FAKE_FUN(arg1, not_matching_arg)).
 
-unload_should_restore_stubbed_module_to_original_test() ->
+unload_should_restore_stubbed_module_to_original() ->
     stub(?REAL_MOD, []),
+
     unload(?REAL_MOD),
+
     ?assertMatch({module, ?REAL_MOD}, code:ensure_loaded(?REAL_MOD)),
     ?assertEqual({?REAL_FUN, 1, 2}, ?REAL_MOD:?REAL_FUN(1,2)).
 
-stub_should_return_expected_value_when_args_match_test() ->
+stub_should_return_expected_value_when_args_match() ->
     stub(?REAL_MOD, [
             expect(?REAL_FUN,[arg1,arg2],
                 and_return(a_result))
         ]),
 
-    ?assertEqual(a_result, ?REAL_MOD:?REAL_FUN(arg1, arg2)),
-    unload(?REAL_MOD).
+    ?assertEqual(a_result, ?REAL_MOD:?REAL_FUN(arg1, arg2)).
 
-stub_should_throw_expected_value_when_args_match_test() ->
+stub_should_throw_expected_value_when_args_match() ->
     stub(?REAL_MOD, [
             expect(?REAL_FUN,[arg1,arg2],
                 and_throw(an_error))
         ]),
 
-    ?assertError(an_error, ?REAL_MOD:?REAL_FUN(arg1, arg2)),
-    unload(?REAL_MOD).
+    ?assertError(an_error, ?REAL_MOD:?REAL_FUN(arg1, arg2)).
 
-stub_should_return_default_value_when_args_do_not_match_test() ->
+stub_should_return_default_value_when_args_do_not_match() ->
     stub(?REAL_MOD, [
             expect(?REAL_FUN,[arg1,arg2],
                 and_return(a_result))
         ]),
 
-    ?assertEqual(ok, ?REAL_MOD:?REAL_FUN(arg1, not_matching_arg)),
-    unload(?REAL_MOD).
+    ?assertEqual(ok, ?REAL_MOD:?REAL_FUN(arg1, not_matching_arg)).
 
-unload_should_throw_when_module_not_hoaxed_test() ->
+unload_should_throw_when_module_not_hoaxed() ->
     ?assertError({not_hoaxed, ?REAL_MOD}, unload(?REAL_MOD)).
 
-unload_0_should_unload_all_hoaxed_modules_test() ->
+unload_0_should_unload_all_hoaxed_modules() ->
     fake(?FAKE_MOD, []),
     stub(?REAL_MOD, []),
 
