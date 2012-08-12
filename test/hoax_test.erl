a8eb50210c0f98be739d19b14dac1e684142897d
Matt Campbell <xenolinguist@gmail.com>
2012-08-12 14:15:14 -0500

    Cleanup tests (still failing)

--- test/hoax_test.erl
+++ test/hoax_test.erl
@@ -5,101 +5,116 @@
 -include_lib("eunit/include/eunit.hrl").
 -include_lib("hoax/include/hoax.hrl").
 
--define(FAKE_MOD, a_nonexistent_module).
--define(FAKE_FUN, nonexistent_function).
--define(REAL_MOD, hoax_test_module).
--define(REAL_FUN, exported_function).
-
 ?HOAX_FIXTURE.
 
-stub_should_throw_when_module_cannot_be_loaded() ->
-    ExpectedError = {no_such_module_to_stub, ?FAKE_MOD},
-    ?assertError(ExpectedError, stub(?FAKE_MOD, [])).
-
-stub_should_throw_when_module_does_not_have_expected_function() ->
-    ExpectedError = {no_such_function_to_stub, {?FAKE_FUN, 0}},
-    ?assertError(ExpectedError,
-        stub(?REAL_MOD, [
-                expect(?FAKE_FUN, [])
-            ])).
-
-fake_should_throw_when_module_exists() ->
-    ExpectedError = {module_exists, ?REAL_MOD},
-    ?assertError(ExpectedError, fake(?REAL_MOD, [])).
-
 unload_should_remove_faked_module_from_vm() ->
-    fake(?FAKE_MOD, []),
+    fake(no_such_module, []),
 
-    unload(?FAKE_MOD),
+    unload(no_such_module),
 
-    ?assertMatch({error,nofile}, code:ensure_loaded(?FAKE_MOD)).
+    Loaded = code:ensure_loaded(no_such_module),
 
-fake_should_return_expected_value_when_args_match() ->
-    fake(?FAKE_MOD, [
-            expect(?FAKE_FUN,[arg1,arg2],
-                and_return(a_result))
-        ]),
-
-    ?assertEqual(a_result, ?FAKE_MOD:?FAKE_FUN(arg1, arg2)).
-
-fake_should_throw_expected_value_when_args_match() ->
-    fake(?FAKE_MOD, [
-            expect(?FAKE_FUN,[arg1,arg2],
-                and_throw(an_error))
-        ]),
-
-    ?assertError(an_error, ?FAKE_MOD:?FAKE_FUN(arg1, arg2)).
-
-fake_should_return_default_value_when_args_do_not_match() ->
-    fake(?FAKE_MOD, [
-            expect(?FAKE_FUN,[arg1,arg2],
-                and_return(a_result))
-        ]),
-
-    ?assertEqual(ok, ?FAKE_MOD:?FAKE_FUN(arg1, not_matching_arg)).
+    ?assertMatch({error,nofile}, Loaded).
 
 unload_should_restore_stubbed_module_to_original() ->
-    stub(?REAL_MOD, []),
+    stub(hoax_test_module, []),
 
-    unload(?REAL_MOD),
+    unload(hoax_test_module),
 
-    ?assertMatch({module, ?REAL_MOD}, code:ensure_loaded(?REAL_MOD)),
-    ?assertEqual({?REAL_FUN, 1, 2}, ?REAL_MOD:?REAL_FUN(1,2)).
+    Loaded = code:ensure_loaded(hoax_test_module),
+    ?assertMatch({module, hoax_test_module}, Loaded),
 
-stub_should_return_expected_value_when_args_match() ->
-    stub(?REAL_MOD, [
-            expect(?REAL_FUN,[arg1,arg2],
-                and_return(a_result))
-        ]),
-
-    ?assertEqual(a_result, ?REAL_MOD:?REAL_FUN(arg1, arg2)).
-
-stub_should_throw_expected_value_when_args_match() ->
-    stub(?REAL_MOD, [
-            expect(?REAL_FUN,[arg1,arg2],
-                and_throw(an_error))
-        ]),
-
-    ?assertError(an_error, ?REAL_MOD:?REAL_FUN(arg1, arg2)).
-
-stub_should_return_default_value_when_args_do_not_match() ->
-    stub(?REAL_MOD, [
-            expect(?REAL_FUN,[arg1,arg2],
-                and_return(a_result))
-        ]),
-
-    ?assertEqual(ok, ?REAL_MOD:?REAL_FUN(arg1, not_matching_arg)).
+    Result = hoax_test_module:exported_function(1, 2),
+    ?assertEqual({exported_function, 1, 2}, Result).
 
 unload_should_throw_when_module_not_hoaxed() ->
-    ?assertError({not_hoaxed, ?REAL_MOD}, unload(?REAL_MOD)).
+    ?assertError({not_hoaxed, hoax_test_module}, unload(hoax_test_module)).
 
 unload_0_should_unload_all_hoaxed_modules() ->
-    fake(?FAKE_MOD, []),
-    stub(?REAL_MOD, []),
+    fake(no_such_module, []),
+    stub(hoax_test_module, []),
 
     unload(),
 
-    ?assertMatch({error,nofile}, code:ensure_loaded(?FAKE_MOD)),
-    ?assertMatch({module, ?REAL_MOD}, code:ensure_loaded(?REAL_MOD)),
-    ?assertEqual({?REAL_FUN, 1, 2}, ?REAL_MOD:?REAL_FUN(1,2)).
+    IsFakeLoaded = code:ensure_loaded(no_such_module),
+    ?assertMatch({error,nofile}, IsFakeLoaded),
+
+    IsRealLoaded = code:ensure_loaded(hoax_test_module),
+    ?assertMatch({module, hoax_test_module}, IsRealLoaded),
+
+    Result = hoax_test_module:exported_function(1, 2),
+    ?assertEqual({exported_function, 1, 2}, Result).
+
+stub_should_throw_when_module_cannot_be_loaded() ->
+    ExpectedError = {no_such_module_to_stub, no_such_module},
+    ?assertError(ExpectedError, stub(no_such_module, [])).
+
+stub_should_throw_when_module_does_not_have_expected_function() ->
+    ExpectedError = {no_such_function_to_stub, {no_such_function, 0}},
+    ?assertError(ExpectedError,
+                 stub(hoax_test_module, [
+                                         expect(no_such_function, [])
+                                        ])).
+
+stub_should_return_expected_value_when_args_match() ->
+    stub(hoax_test_module, [
+                            expect(exported_function,[arg1,arg2],
+                                   and_return(a_result))
+                           ]),
+
+    Result = hoax_test_module:exported_function(arg1, arg2),
+
+    ?assertEqual(a_result, Result).
+
+stub_should_throw_expected_value_when_args_match() ->
+    stub(hoax_test_module, [
+                            expect(exported_function,[arg1,arg2],
+                                   and_throw(an_error))
+                           ]),
+
+    ?assertError(an_error,
+                 hoax_test_module:exported_function(arg1, arg2)).
+
+stub_should_return_default_value_when_args_do_not_match() ->
+    stub(hoax_test_module, [
+                            expect(exported_function,[arg1,arg2],
+                                   and_return(a_result))
+                           ]),
+
+    Result = hoax_test_module:exported_function(arg1, not_matching_arg),
+
+    ?assertEqual(ok, Result).
+
+fake_should_throw_when_module_exists() ->
+    ExpectedError = {module_exists, hoax_test_module},
+    ?assertError(ExpectedError, fake(hoax_test_module, [])).
+
+fake_should_return_expected_value_when_args_match() ->
+    fake(no_such_module, [
+                                expect(no_such_function,[arg1,arg2],
+                                       and_return(a_result))
+                               ]),
+
+    Result = no_such_module:no_such_function(arg1, arg2),
+
+    ?assertEqual(a_result, Result).
+
+fake_should_throw_expected_value_when_args_match() ->
+    fake(no_such_module, [
+                                expect(no_such_function,[arg1,arg2],
+                                       and_throw(an_error))
+                               ]),
+
+    ?assertError(an_error,
+                 no_such_module:no_such_function(arg1, arg2)).
+
+fake_should_return_default_value_when_args_do_not_match() ->
+    fake(no_such_module, [
+                                expect(no_such_function,[arg1,arg2],
+                                       and_return(a_result))
+                               ]),
+
+    Result = no_such_module:no_such_function(arg1, not_matching_arg),
+
+    ?assertEqual(ok, Result).
 
