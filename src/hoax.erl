fd165847efd6bb788792d5fb52ce8e20bb4f0214
Matt Campbell <xenolinguist@gmail.com>
2012-08-11 21:13:31 -0500

    Add fake/2 and unload/1

--- src/hoax.erl
+++ src/hoax.erl
@@ -1,25 +1,34 @@
 -module(hoax).
 
 -export([stub/1, stub/2,
-        %fake/2,
+        fake/2,
         stub_a/2, stub_a/3,
         expect/2,
         expect/3,
         and_return/1,
         and_throw/1,
-        unload/0
+        unload/1
     ]).
 
 stub(M) -> stub(M, []).
 stub(ModuleName, Expectations) ->
     ModName = list_to_atom(atom_to_list(ModuleName) ++ "_hoax"),
     Funcs = get_exports(ModuleName),
-    compile(hoax_ast:module(ModName, Funcs, Expectations)).
+    mock(ModName, Funcs, Expectations).
+
+fake(ModuleName, Expectations) ->
+    case module_exists(ModuleName) of
+        true ->
+            erlang:error({module_exists, ModuleName});
+        false ->
+            Funcs = [{F,length(A)} || {F,A,_} <- Expectations],
+            mock(ModuleName, Funcs, Expectations)
+    end.
 
 stub_a(B, M) -> stub_a(B, M, []).
 stub_a(Behaviour, ModuleName, Expectations) ->
     Funcs = Behaviour:behaviour_info(callbacks),
-    compile(hoax_ast:module(ModuleName, Funcs, Expectations)).
+    mock(ModuleName, Funcs, Expectations).
 
 expect(Func, Args) -> expect(Func, Args, and_return(ok)).
 expect(Func, Args, Action) -> {Func, Args, Action}.
@@ -28,20 +37,29 @@ and_return(Value) -> {return, Value}.
 
 and_throw(Error) -> {throw, Error}.
 
-unload() -> ok.
+unload(ModuleName) ->
+    code:delete(ModuleName),
+    code:purge(ModuleName).
 
 %%%%%%%%%%%%%
 
-compile(AST) ->
+mock(ModuleName, Funcs, Expectations) ->
+    AST = hoax_ast:module(ModuleName, Funcs, Expectations),
     Forms = erl_syntax:revert_forms(AST),
     {ok, Mod, Bin} = compile:forms(Forms),
     code:load_binary(Mod, "", Bin).
 
 get_exports(ModuleName) ->
-    case code:ensure_loaded(ModuleName) of
-        {error, nofile} ->
+    case module_exists(ModuleName) of
+        false ->
             erlang:error({no_such_module_to_stub, ModuleName});
-        {module, ModuleName} ->
-            true = code:unstick_mod(ModuleName),
+        true ->
             [ E || E = {F,_} <- ModuleName:module_info(exports), F =/= module_info ]
     end.
+
+module_exists(ModuleName) ->
+    case code:ensure_loaded(ModuleName) of
+        {error, nofile} -> false;
+        {module, ModuleName} -> true
+    end.
+
--- test/hoax_test.erl
+++ test/hoax_test.erl
@@ -1,7 +1,7 @@
 -module(hoax_test).
 
 -compile([export_all]).
--import(hoax, [stub/2, expect/2, expect/3, and_return/1]).
+-import(hoax, [stub/2, expect/2, expect/3, and_return/1, fake/2, unload/1]).
 
 -include_lib("eunit/include/eunit.hrl").
 
@@ -20,3 +20,29 @@ stub_should_throw_when_module_does_not_have_expected_function_test() ->
                 expect(?FAKE_FUN, [])
             ])).
 
+fake_should_throw_when_module_exists_test() ->
+    ExpectedError = {module_exists, ?REAL_MOD},
+    ?assertError(ExpectedError, fake(?REAL_MOD, [])).
+
+unload_should_remove_faked_module_from_vm_test() ->
+    fake(?FAKE_MOD, []),
+    unload(?FAKE_MOD),
+    ?assertMatch({error,nofile}, code:ensure_loaded(?FAKE_MOD)).
+
+fake_should_return_expected_value_when_args_match_test() ->
+    fake(?FAKE_MOD, [
+            expect(?FAKE_FUN,[arg1,arg2],
+                and_return(a_result))
+        ]),
+
+    ?assertEqual(a_result, ?FAKE_MOD:?FAKE_FUN(arg1, arg2)),
+    unload(?FAKE_MOD).
+
+fake_should_return_default_value_when_args_do_not_match_test() ->
+    fake(?FAKE_MOD, [
+            expect(?FAKE_FUN,[arg1,arg2],
+                and_return(a_result))
+        ]),
+
+    ?assertEqual(ok, ?FAKE_MOD:?FAKE_FUN(arg1, not_matching_arg)),
+    unload(?FAKE_MOD).
