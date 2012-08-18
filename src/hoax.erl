8433fdb5586e6247978a06476ff5658740665fe1
Matt Campbell <xenolinguist@gmail.com>
2012-08-18 15:07:19 -0500

    Remove app infrastructure

--- src/hoax.erl
+++ src/hoax.erl
@@ -4,21 +4,6 @@
 -export(?HOAX_API).
 -ignore_xref(?HOAX_API).
 
--behaviour(application).
-
-%% Application callbacks
--export([start/2, stop/1]).
-
-%% ===================================================================
-%% Application callbacks
-%% ===================================================================
-
-start(_StartType, _StartArgs) ->
-    hoax_sup:start_link().
-
-stop(_State) ->
-    ok.
-
 %% ===================================================================
 %% hoax API
 %% ===================================================================
