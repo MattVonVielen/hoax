648951909fb7fb7d9309618e8f95d1664f7ae9dd
Matt Campbell <xenolinguist@gmail.com>
2015-05-17 17:23:17 -0500

    Remove unused code

--- test/hoax_fun_tests.erl
+++ test/hoax_fun_tests.erl
@@ -4,19 +4,6 @@
 
 -compile(export_all).
 
-should_find_name_in_module_test_() ->
-    [
-     ?_assertEqual(setup, hoax_fun:find_name_in_module(?MODULE, fun hoax_fun_tests:setup/0 )),
-     ?_assertEqual(teardown, hoax_fun:find_name_in_module(?MODULE, fun hoax_fun_tests:teardown/1 ))
-    ].
-
-setup() ->
-    ok.
-
-teardown(_) ->
-    ok.
-
-
 should_find_arg_count_for_fun_test() ->
     TestFun = fun(_Arg1, _Arg2, _Arg3) ->
                       ok
