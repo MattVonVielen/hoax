147dddc1e8e69bb76447111a1e748632119ce855
Matt Campbell <xenolinguist@gmail.com>
2015-06-12 16:58:03 -0500

    Use hoax:test/1 instead of try/after/end

--- test/hoax_tests.erl
+++ test/hoax_tests.erl
@@ -7,35 +7,28 @@
 
 stop_should_unload_all_hoaxed_modules_test() ->
     ExpectedResult = hoax_test_module:function_one(1, 2),
-    hoax:start(),
-
-    try
+    hoax:test(fun() ->
         mock(no_such_module, expect_no_interactions),
         mock(hoax_test_module, ?expect(function_one, ?withArgs([1, 2]), ?andReturn(mocked_return_value))),
 
         ?assertEqual(mocked_return_value, hoax_test_module:function_one(1, 2))
-    after
-        hoax:stop()
-    end,
+    end),
 
     Result = hoax_test_module:function_one(1, 2),
     ?assertEqual(ExpectedResult, Result),
 
-    ?assertMatch({error,nofile}, code:ensure_loaded(no_such_module)).
+    ?assertMatch({error, nofile}, code:ensure_loaded(no_such_module)).
 
 should_be_able_to_mock_sticky_modules_test() ->
     ExpectedResult = hoax_test_module:function_one(1, 2),
     code:stick_mod(hoax_test_module),
     try
-        hoax:start(),
-
-        try
+        hoax:test(fun() ->
             mock(hoax_test_module, ?expect(function_one, ?withArgs([1, 2]), ?andReturn(mocked_return_value))),
             ?assertEqual(mocked_return_value, hoax_test_module:function_one(1, 2)),
             ?assert(code:is_sticky(hoax_test_module))
-        after
-            hoax:stop()
-        end,
+        end),
+
         Result = hoax_test_module:function_one(1, 2),
         ?assertEqual(ExpectedResult, Result),
         ?assert(code:is_sticky(hoax_test_module))
@@ -44,15 +37,15 @@ should_be_able_to_mock_sticky_modules_test() ->
     end.
 
 full_stack_fun_expectation_test() ->
-    hoax:start(),
-    Expected = 1,
-    mock(hoax_test_module, ?expect(function_two, fun(Val) ->
-                                  ?assertEqual(Expected, Val),
-                                  local_return
-                                  end)),
+    hoax:test(fun() ->
+        Expected = 1,
+        mock(hoax_test_module, ?expect(function_two, fun(Val) ->
+            ?assertEqual(Expected, Val),
+            local_return
+        end)),
 
-    ?assertEqual(local_return, hoax_test_module:function_two(Expected)),
-    hoax:stop().
+        ?assertEqual(local_return, hoax_test_module:function_two(Expected))
+    end).
 
 -define(EXAMPLE_MODULE, module_for_testing_fixtures).
 
@@ -65,7 +58,7 @@ fixture_for_entire_module_without_setup_and_teardown_test() ->
 
     {foreach, _, _, Tests} = hoax:fixture(?EXAMPLE_MODULE),
 
-    ?assertEqual(ExpectedSortedFunctions, lists:sort([F || {_,F} <- Tests])).
+    ?assertEqual(ExpectedSortedFunctions, lists:sort([F || {_, F} <- Tests])).
 
 fixture_for_entire_module_with_setup_and_teardown_test() ->
     ExpectedSortedFunctions = [
@@ -76,7 +69,7 @@ fixture_for_entire_module_with_setup_and_teardown_test() ->
 
     {foreach, _, _, Tests} = hoax:fixture(?EXAMPLE_MODULE, setup, teardown),
 
-    ?assertEqual(ExpectedSortedFunctions, lists:sort([F || {_,F} <- Tests])).
+    ?assertEqual(ExpectedSortedFunctions, lists:sort([F || {_, F} <- Tests])).
 
 fixture_for_prefix_without_setup_and_teardown_test() ->
     ExpectedSortedPrefix1Functions = [
@@ -89,8 +82,8 @@ fixture_for_prefix_without_setup_and_teardown_test() ->
     {foreach, _, _, Prefix1Tests} = hoax:fixture(?EXAMPLE_MODULE, prefix1),
     {foreach, _, _, Prefix2Tests} = hoax:fixture(?EXAMPLE_MODULE, prefix2),
 
-    ?assertEqual(ExpectedSortedPrefix1Functions, lists:sort([F || {_,F} <- Prefix1Tests])),
-    ?assertEqual(ExpectedSortedPrefix2Functions, lists:sort([F || {_,F} <- Prefix2Tests])),
+    ?assertEqual(ExpectedSortedPrefix1Functions, lists:sort([F || {_, F} <- Prefix1Tests])),
+    ?assertEqual(ExpectedSortedPrefix2Functions, lists:sort([F || {_, F} <- Prefix2Tests])),
 
     {foreach, _, _, Prefix1TestsFromString} = hoax:fixture(?EXAMPLE_MODULE, "prefix1"),
     {foreach, _, _, Prefix2TestsFromString} = hoax:fixture(?EXAMPLE_MODULE, "prefix2"),
@@ -109,8 +102,8 @@ fixture_for_prefix_with_setup_and_teardown_test() ->
     {foreach, _, _, Prefix1Tests} = hoax:fixture(?EXAMPLE_MODULE, prefix1, prefix1_setup, prefix1_teardown),
     {foreach, _, _, Prefix2Tests} = hoax:fixture(?EXAMPLE_MODULE, prefix2, prefix2_setup, prefix2_teardown),
 
-    ?assertEqual(ExpectedSortedPrefix1Functions, lists:sort([F || {_,F} <- Prefix1Tests])),
-    ?assertEqual(ExpectedSortedPrefix2Functions, lists:sort([F || {_,F} <- Prefix2Tests])),
+    ?assertEqual(ExpectedSortedPrefix1Functions, lists:sort([F || {_, F} <- Prefix1Tests])),
+    ?assertEqual(ExpectedSortedPrefix2Functions, lists:sort([F || {_, F} <- Prefix2Tests])),
 
     {foreach, _, _, Prefix1TestsFromString} = hoax:fixture(?EXAMPLE_MODULE, "prefix1", prefix1_setup, prefix1_teardown),
     {foreach, _, _, Prefix2TestsFromString} = hoax:fixture(?EXAMPLE_MODULE, "prefix2", prefix2_setup, prefix2_teardown),
